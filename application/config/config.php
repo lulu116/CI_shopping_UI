85c74cc5337667f5c3194202f6f2d3fc15d2b907
lulu116 <sliusting970@163.com>
2017-11-10 17:14:30 +0800

    更改控制器内容

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://192.168.4.110:80/lulu_githubproject';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
